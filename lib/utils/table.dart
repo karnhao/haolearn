f68b2b404f9e89b7e549840990669fcaa660799b
karnhao <karn0888422952@gmail.com>
2022-11-22 14:50:26 +0700

    add subject name in table

--- lib/utils/table.dart
+++ lib/utils/table.dart
@@ -13,31 +13,38 @@ Widget createTableWidget(BuildContext context) {
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       color: Colors.amber[100],
-      child: TimePlanner(
-        currentTimeAnimation: true,
-        startHour: 6,
-        endHour: 21,
-        headers: const [
-          TimePlannerTitle(title: "SUN"),
-          TimePlannerTitle(title: "MON"),
-          TimePlannerTitle(title: "TUE"),
-          TimePlannerTitle(title: "WED"),
-          TimePlannerTitle(title: "TRU"),
-          TimePlannerTitle(title: "FRI"),
-          TimePlannerTitle(title: "SAT"),
-        ],
-        style: TimePlannerStyle(
-          backgroundColor: const Color.fromARGB(255, 253, 119, 164),
+      child: Center(
+        child: TimePlanner(
+          currentTimeAnimation: true,
+          startHour: 6,
+          endHour: 21,
+          headers: const [
+            TimePlannerTitle(title: "SUN"),
+            TimePlannerTitle(title: "MON"),
+            TimePlannerTitle(title: "TUE"),
+            TimePlannerTitle(title: "WED"),
+            TimePlannerTitle(title: "TRU"),
+            TimePlannerTitle(title: "FRI"),
+            TimePlannerTitle(title: "SAT"),
+          ],
+          style: TimePlannerStyle(
+            cellWidth: 200,
+            backgroundColor: const Color.fromARGB(255, 253, 119, 164),
+          ),
+          tasks: allSubjectTables
+              .where((u) => !u.isEmptySubject)
+              .map((t) => TimePlannerTask(
+                    dateTime: TimePlannerDateTime(
+                        day: t.time.day,
+                        hour: t.time.getStartHour(),
+                        minutes: t.time.getStartMinute()),
+                    minutesDuration: t.time.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Text(t.name)],
+                    ),
+                  ))
+              .toList(),
         ),
-        tasks: allSubjectTables
-            .where((u) => !u.isEmptySubject)
-            .map((t) => TimePlannerTask(
-                  dateTime: TimePlannerDateTime(
-                      day: t.time.day,
-                      hour: t.time.getStartHour(),
-                      minutes: t.time.getStartMinute()),
-                  minutesDuration: t.time.width,
-                ))
-            .toList(),
       ));
 }
